902415a4793d592d35b42c449daa056bbe61881e
Amy Cheng <amylynncheng@gmail.com>
2017-06-15 14:45:33 -0500

    Create index.js

--- index.js
+++ index.js
@@ -113,14 +113,14 @@ restService.post('/reply', function (req, res) {
             });
 
         case "food.plate":                
-                    var decider = Math.random() * vegetables.length;
-                    var index = Math.floor(decider);
-                    var text = "I recommend adding " + vegetables[index] + " to your plate.";
-                    return res.json({
-                        speech: text,
-                        displayText: text,
-                        source: "survey-demo-app"
-                    });
+            var decider = Math.random() * vegetables.length;
+            var index = Math.floor(decider);
+            var text = "I recommend adding " + vegetables[index] + " to your plate.";
+            return res.json({
+                speech: text,
+                displayText: text,
+                source: "survey-demo-app"
+            });
 
         default:
             return res.json({
