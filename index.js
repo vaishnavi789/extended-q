7d30737589633bfdf7837e347369fd6ddeab3521
Amy Cheng <amylynncheng@gmail.com>
2017-06-19 15:01:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ restService.use(bodyParser.json());
 
 restService.post('/reply', function (req, res) {
     var action = req.body.result.action;
-    var previous_action = req.body.result.parameters.monitor-action;
+    var previous_action = req.body.result.parameters.monitorAction;
     var text;
     
     switch (action) {
