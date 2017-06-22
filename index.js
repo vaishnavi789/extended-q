ee32457e97f11bb505077471414d58ef6863128b
Amy Cheng <amylynncheng@gmail.com>
2017-06-22 15:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 
-var monitoring = [ 
+var monitoring = [
     {
         question: "Okay, have you eaten within the past 90 minutes?",
         type: "yesno"
@@ -54,7 +54,7 @@ var coping = [
         type: "frequency"
     },
     {
-        question: "Can you usually sit at ease and feel relaxed?",
+        question: "Can you sit at ease and feel relaxed?",
         type: "yesno"
     },
     {
@@ -69,7 +69,8 @@ var copingCount = 0;
 var vegetables = ["chillies", "carrots", "cabbage", "eggplant", "cauliflower", "broccoli", "tomatoes", "spinach", "peppers"];
 var starches = ["brown rice", "squash", "green peas", "yams", "sweet potato", "pasta", "potatoes"];
 var proteins = ["chicken", "beans", "fish", "crab", "shrimp", "eggs", "turkey", "beef", "pork"];
-var answers = [];
+var monitorAnswers = [];
+var copeAnswers = [];
 
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -81,57 +82,61 @@ restService.post('/reply', function (req, res) {
     var action = req.body.result.action;
     //var previous_action = req.body.result.parameters.monitorAction;
     var text;
-    
+
     switch (action) {
         case "monitoring.continue":
           action = "start.monitor";
-           
-            
+
+
         case "start.monitor":
           if (monitorCount >= monitoring.length) {
-              answers.push(req.body.result.resolvedQuery);  
+              monitorAnswers.push(req.body.result.resolvedQuery);
               monitorCount = 0;
-              
-              var ate = answers[0];
-              var sugarLevel = answers[1];
-              var medication = answers[2];
-              var exercise = answers[3];
-              var weight = answers[4];
-              
-              console.log(sugarLevel);
-              console.log(ate);
-              console.log(answers);
-              text = "I'll get this logged for you ASAP. " 
-                +  monitorResult(ate, sugarLevel, exercise, weight)
-                + "What else can I do for you?";
+
+              var ate = monitorAnswers[0];
+              var sugarLevel = monitorAnswers[1];
+              var medication = monitorAnswers[2];
+              var exercise = monitorAnswers[3];
+              var weight = monitorAnswers[4];
+
+              text = "I'll get this logged for you ASAP. "
+                +  monitorResult(ate, sugarLevel, exercise, weight);
+                //+ "What else can I do for you?";
               break;
           }
           text = monitoring[monitorCount].question;
-          
+
           if (req.body.result.parameters.number.length != 0) {
-                answers.push(req.body.result.parameters.number); 
+                monitorAnswers.push(req.body.result.parameters.number);
           } else if (req.body.result.parameters.yesno.length != 0) {
-                answers.push(req.body.result.parameters.yesno);            
+                monitorAnswers.push(req.body.result.parameters.yesno);
           }
-            
-          //answers.push(req.body.result.resolvedQuery);  
-                
+
           monitorCount++;
           break;
-            
-        case "coping.continue":     
+
+        case "coping.continue":
             action = "start.coping";
-            
+
         case "start.coping":
           if (copingCount >= coping.length) {
               copingCount = 0;
-              text = "Thank you! That's all the questions. What else can I help you with?";
+
+              text = "Thank you for answering my questions. ";
+                //+ copingResults(copeAnswers);
               break;
           }
           text = coping[copingCount].question;
+
+          if (req.body.result.parameters.frequency.length != 0) {
+                copeAnswers.push(req.body.result.parameters.number);
+          } else if (req.body.result.parameters.yesno.length != 0) {
+                copeAnswers.push(req.body.result.parameters.yesno);
+          }
+
           copingCount++;
           break;
-            
+
       //dietary advice action based on the diabetes.org "food plate" page.
         case "food.plate":
           var vDecider = Math.random() * vegetables.length;
@@ -144,19 +149,19 @@ restService.post('/reply', function (req, res) {
                 + ", 1/4 with " + starches[sIndex] + " , and 1/4 with " + proteins[pIndex]
                 + ". If you want to change the plate, just say \"make another plate\".";;
           break;
-            
-        case "restart": 
+
+        case "restart":
            monitorCount = 0;
            copingCount = 0;
-           answers = [];
-           text = "Sure thing. I've reset all the surveys so you can start from the beginning. What would you like to do now?";  
+           monitorAnswers = [];
+           text = "Sure thing. I've reset all the surveys so you can start from the beginning. What would you like to do now?";
            break;
-        
+
         case "help":
             text = "I can assist you with monitoring your health, emotional coping with your diabetes, and food recommendations."
                 +   " Just say any of the key words and we can get started!";
             break;
-            
+
         default:
           text = "Error. Could not find appropriate action.";
     }
@@ -177,11 +182,30 @@ function monitorResult (ate, sugar, exercise, weight) {
     } else if (ate == "no" && sugar >= 4 && sugar <= 7) {
         result += "Your blood sugar level of " + sugar + " is normal. Keep it up! ";
     } else {
-        result += "Your blood sugar is too low. I suggest eating a small amount of carbs. "; 
+        result += "Your blood sugar is too low. I suggest eating a small amount of carbs. ";
+    }
+
+    return result;
+}
+/*
+function copingResult (answers) {
+    var score = 0;
+    var result = "";
+    for (int = 0; i < answers.length; i++) {
+        if (answers[i] == "yes") {
+            score++;
+        } else if (answers[i] == "often") {
+            score++;
+        }
+    }
+    
+    if (score > 19 && score <= 27) {
+        result += "You are showing signs of severe depression. Please consider asking your doctor for help. ";
     }
     
     return result;
 }
+*/
 
 restService.get('/', function (req, res) {
     return "Hello and welcome.";
