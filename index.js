0c050372a5c1faf615649b02321929dd092f5792
Amy Cheng <amylynncheng@gmail.com>
2017-06-22 13:28:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ restService.post('/reply', function (req, res) {
             
           //answers.push(JSON.stringify(req.body.result.resolvedQuery));
           answers.push(req.body.result.resolvedQuery);  
- 
+           console.log(req.body.result.parameters);
                 
           monitorCount++;
           break;
